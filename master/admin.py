61c99f7abbd3a51c2ccf62fbc07627466878876a
Bagas Julianto <bagasjulianto212@gmail.com>
2024-06-04 20:57:53 +0700

    Done: point 3

--- master/admin.py
+++ master/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from master.models import Country, Category
 
-# Register your models here.
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("name", "flag", "currency")
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("country", "title", "price_per_kilo")
+
+
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Category, CategoryAdmin)
--- master/models.py
+++ master/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=255, null=False, blank=False)
-    flag = models.CharField(max_length=255, null=False, blank=False)
-    currency = models.CharField(max_length=255, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
+    flag = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+        unique=True,
+        help_text="static url for this flag",
+    )
+    currency = models.CharField(max_length=255, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = "Country"
@@ -15,6 +24,9 @@ class Category(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, null=False, blank=False)
     price_per_kilo = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name = "Category"
