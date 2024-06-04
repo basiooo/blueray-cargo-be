6f7ea7283fb6692a98fcd93e83d5e757e672175e
Bagas Julianto <bagasjulianto212@gmail.com>
2024-06-04 22:26:51 +0700

    display flag as image in django admin

--- master/admin.py
+++ master/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from master.models import Country, Category
+from django.utils.html import mark_safe
 
 
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ("name", "flag", "currency")
+    list_display = ("name", "get_flag", "currency")
+
+    def get_flag(self, obj):
+        return mark_safe(f"<img src='{obj.flag}' alt='{obj.flag}'>")
+
+    get_flag.short_description = "flag"
 
 
 class CategoryAdmin(admin.ModelAdmin):
