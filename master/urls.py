f771607e97dd9ac38563b24b9bea7cd17ae48768
Bagas Julianto <bagasjulianto212@gmail.com>
2024-06-07 03:10:48 +0700

    fix url

--- master/urls.py
+++ master/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from master.views import CountryListAV, CategoryListAV, destination, calculation
 
 api_url_pattern = [
-    path("api/countries", CountryListAV.as_view(), name="api_countries"),
-    path("api/categories", CategoryListAV.as_view(), name="api_categories"),
-    path("api/destination", destination, name="api_destination"),
-    path("api/calculation", calculation, name="api_calculation"),
+    path("api/countries/", CountryListAV.as_view(), name="api_countries"),
+    path("api/categories/", CategoryListAV.as_view(), name="api_categories"),
+    path("api/destination/", destination, name="api_destination"),
+    path("api/calculation/", calculation, name="api_calculation"),
 ]
